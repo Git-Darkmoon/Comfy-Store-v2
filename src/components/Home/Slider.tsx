d26df85fdd7f79887838fe527be58ae90244b3a7
Nicolas Mayorga <namayorgav@gmail.com>
2024-05-30 18:53:48 -0500

    [ADD] Fully working responsive carousel slider.

--- src/components/Home/Slider.tsx
+++ src/components/Home/Slider.tsx
@@ -1,19 +1,39 @@
 import SliderCard from "./SliderCard"
 
+const SliderData = [
+  {
+    id: 1,
+    logo: "/assets/carousel/logoipsum001.webp",
+  },
+  {
+    id: 2,
+    logo: "/assets/carousel/logoipsum002.webp",
+  },
+  {
+    id: 3,
+    logo: "/assets/carousel/logoipsum003.webp",
+  },
+  {
+    id: 4,
+    logo: "/assets/carousel/logoipsum004.webp",
+  },
+  {
+    id: 5,
+    logo: "/assets/carousel/logoipsum005.webp",
+  },
+  {
+    id: 6,
+    logo: "/assets/carousel/logoipsum006.webp",
+  },
+]
+
 function Slider() {
   return (
-    <div className="carousel__wrapper">
-      <div className="carousel">
-        <SliderCard />
-        <SliderCard />
-        <SliderCard />
-        <SliderCard />
-        <SliderCard />
-        <SliderCard />
-        <SliderCard />
-        <SliderCard />
-      </div>
-    </div>
+    <section className="carousel">
+      {SliderData.map((sliderItem) => {
+        return <SliderCard key={sliderItem.id} {...sliderItem} />
+      })}
+    </section>
   )
 }
 export default Slider
--- src/components/Home/SliderCard.tsx
+++ src/components/Home/SliderCard.tsx
@@ -1,4 +1,14 @@
-function SliderCard() {
-  return <div className="carousel__slide">SliderCard</div>
+import Image from "next/image"
+
+type SliderCardProps = {
+  logo: string
+}
+
+function SliderCard({ logo }: SliderCardProps) {
+  return (
+    <article className="carousel__item">
+      <Image src={logo} alt="Collab branding" width={240} height={100} />
+    </article>
+  )
 }
 export default SliderCard
