e84053b56735987a55d899aa0e36cfa61df90b5a
Nicolas Mayorga <namayorgav@gmail.com>
2024-06-06 19:17:51 -0500

    [ADD] Reusable component.

--- src/components/Account/AccountForm.tsx
+++ src/components/Account/AccountForm.tsx
@@ -1,4 +1,62 @@
-function AccountForm() {
-  return <div>AccountForm</div>
+import Link from "next/link"
+import SectionTitle from "../SectionTitle"
+import AccountFormInput from "./AccountFormInput"
+
+type accountFormProps = {
+  submitHandler: (e: React.FormEvent<HTMLFormElement>) => void
+  variant: "login" | "register"
+}
+
+function AccountForm({ submitHandler, variant }: accountFormProps) {
+  if (variant === "login") {
+    return (
+      <main className="accountFormWrapper">
+        <form onSubmit={submitHandler} className="accountForm">
+          <SectionTitle title="Login" subtitle="Shop your way" />
+          <AccountFormInput
+            key={"email"}
+            id="email"
+            label="email"
+            type="email"
+          />
+          <AccountFormInput
+            key={"password"}
+            id="password"
+            label="Password"
+            type="password"
+          />
+          <button className="accountForm__btn">Login</button>
+          <p>
+            Not a member yet ?<Link href={"/register"}>Register</Link>
+          </p>
+        </form>
+      </main>
+    )
+  }
+
+  return (
+    <main className="accountFormWrapper">
+      <form onSubmit={submitHandler} className="accountForm">
+        <SectionTitle title="Register" subtitle="From now discover luxury" />
+        <AccountFormInput
+          key={"username"}
+          id="username"
+          label="username"
+          type="text"
+        />
+        <AccountFormInput key={"email"} id="email" label="email" type="email" />
+        <AccountFormInput
+          key={"password"}
+          id="password"
+          label="Password"
+          type="password"
+        />
+        <button className="accountForm__btn">Create Account</button>
+        <p>
+          Already a member ?<Link href={"/login"}>Login</Link>
+        </p>
+      </form>
+    </main>
+  )
 }
 export default AccountForm
--- src/components/Account/AccountFormInput.tsx
+++ src/components/Account/AccountFormInput.tsx
@@ -1,11 +1,25 @@
-function AccountFormInput() {
+import { InputHTMLAttributes } from "react"
+
+type accountFormInputProps = {
+  type: HTMLInputElement["type"]
+  id: string
+  label: string
+}
+
+function AccountFormInput({ type, id, label }: accountFormInputProps) {
   return (
-    <div className="input-container">
-      <input required id="input" type="text" />
-      <label className="label" htmlFor="input">
-        Name
+    <div className="accountForm__input-container">
+      <input
+        required
+        id={id}
+        type={type}
+        name={id}
+        className="accountForm__input"
+      />
+      <label className="accountForm__label" htmlFor={id}>
+        {label}
       </label>
-      <div className="underline"></div>
+      <div className="accountForm__input-underline"></div>
     </div>
   )
 }
