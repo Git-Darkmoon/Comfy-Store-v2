e3bf17f5e98b77527d9eaac1d8603451d1ec44b4
Nicolas Mayorga <namayorgav@gmail.com>
2024-07-10 22:21:26 -0500

    styles (filters): Updated to color schema.

--- src/components/Products/Filters.tsx
+++ src/components/Products/Filters.tsx
@@ -1,60 +1,68 @@
+const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  event.preventDefault()
+
+  const formData = new FormData(event.currentTarget)
+  console.log(Object.fromEntries(formData.entries()))
+}
+
 function Filters() {
   return (
     <aside className="products__filters">
-      <input
-        type="search"
-        className="products__search"
-        placeholder="Search"
-        name="SearchProduct"
-        aria-label="Search"
-        role="searchbox"
-      />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          className="products__search"
+          placeholder="Search"
+          name="SearchProduct"
+          aria-label="Search"
+          role="searchbox"
+        />
 
-      <h3>Category</h3>
-      <select name="category" className="products__categorySelector">
-        <option value="all">All</option>
-        <option value="tables">Tables</option>
-        <option value="chairs">Chairs</option>
-        <option value="kids">Kids</option>
-        <option value="sofas">Sofas</option>
-        <option value="beds">Beds</option>
-      </select>
-      <h3>Company</h3>
-      <select name="" className="products__companySelector">
-        <option value="all">All</option>
-        <option value="all">All</option>
-        <option value="all">All</option>
-        <option value="all">All</option>
-        <option value="all">All</option>
-      </select>
-      <h3>Colors</h3>
-      <ul>
-        <li>
-          <button>All</button>
-        </li>
-        <li>
-          <button>All</button>
-        </li>
-        <li>
-          <button>All</button>
-        </li>
-        <li>
-          <button>All</button>
-        </li>
-        <li>
-          <button>All</button>
-        </li>
-      </ul>
-      <h3>Price</h3>
-      <input type="range" min={0} max={4000} />
-      <br />
-      <label htmlFor="freeShipping">Free Shipping</label>
-      <input type="checkbox" name="freeShipping" id="freeShipping" />
-      <br />
-      <button className="products__searchBtn products__btn">Search</button>
-      <button className="products__clearBtn products__btn">
-        Clear Filters
-      </button>
+        <label htmlFor="category">Category</label>
+        <select
+          id="category"
+          name="category"
+          className="products__categorySelector products__select"
+        >
+          <option value="all">All</option>
+          <option value="tables">Tables</option>
+          <option value="chairs">Chairs</option>
+          <option value="kids">Kids</option>
+          <option value="sofas">Sofas</option>
+          <option value="beds">Beds</option>
+        </select>
+        <label htmlFor="company">Company</label>
+        <select
+          id="company"
+          name=""
+          className="products__companySelector products__select"
+        >
+          <option value="all">All</option>
+          <option value="all">All</option>
+          <option value="all">All</option>
+          <option value="all">All</option>
+          <option value="all">All</option>
+        </select>
+        <label htmlFor="sort">Sort By</label>
+        <select
+          name=""
+          id="sort"
+          className="products__companySelector products__select"
+        >
+          <option value="all">A-Z</option>
+          <option value="all">Z-A</option>
+          <option value="all">high</option>
+          <option value="all">low</option>
+        </select>
+        <label htmlFor="">Price</label>
+        <input type="range" min={0} max={4000} />
+        <label htmlFor="freeShipping">Free Shipping</label>
+        <input type="checkbox" name="freeShipping" id="freeShipping" />
+        <button className="products__searchBtn products__btn">Search</button>
+        <button className="products__clearBtn products__btn">
+          Clear Filters
+        </button>
+      </form>
     </aside>
   )
 }
